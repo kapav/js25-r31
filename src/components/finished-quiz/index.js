ffef8adb8a62a7c3437219fcd46c49891af7dfa0
Кочкин Александр <Alexander.Kochkin@artezio.com>
2020-01-29 12:44:27 +0400

    Added the first part of form validation. Lesson 9-3.

--- src/components/finished-quiz/index.js
+++ src/components/finished-quiz/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './index.module.css'
 import Button from '../ui/button'
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -32,7 +33,9 @@ const FinishedQuiz = props => {
       <p>Правильно {successCount} из {props.quiz.length}</p>
       <div>
         <Button onClick={props.onRedo} type="primary">Повторить</Button>
-        <Button type="success">Перейти к списку тестов</Button>
+        <Link to="/">
+          <Button type="success">Перейти к списку тестов</Button>
+        </Link>
       </div>
     </div>
   )
--- src/containers/auth/index.js
+++ src/containers/auth/index.js
@@ -1,10 +1,92 @@
 import React, {Component} from 'react'
+import classes from './index.module.css'
+import Button from '../../components/ui/button'
+import Input from '../../components/ui/input'
 
 export default class Auth extends Component {
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Почта',
+        errorMessage: 'Введите правильную почту',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите правильный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+  loginHandler = () => {
+
+  }
+  registerHandler = () => {
+
+  }
+  submitHandler = event => {
+    event.preventDefault()
+  }
+  onChangeHandler = (event, controlName) => {
+    console.log(`${controlName}:`, event.target.value)
+  }
+  renderInputs() {
+    return Object.keys(this.state.formControls)
+      .map((controlName, index) => {
+        const control = this.state.formControls[controlName]
+        return (
+          <Input
+            key={controlName + index}
+            type={control.type}
+            value={control.value}
+            valid={control.valid}
+            touched={control.touched}
+            label={control.label}
+            shouldValidate={!!control.validation}
+            errorMessage={control.errorMessage}
+            onChange={event => this.onChangeHandler(event, controlName)}
+          />
+        )
+      })
+  }
   render() {
     return (
-      <div>
-        <h1>Авторизация</h1>
+      <div className={classes.auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form
+            onSubmit={this.submitHandler}
+            className={classes.authForm}
+          >
+            { this.renderInputs() }
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Войти
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+            >
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
