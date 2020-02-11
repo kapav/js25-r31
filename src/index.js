d0ff1742ada019e8198735ad434aa69278712b4a
Кочкин Александр <Alexander.Kochkin@artezio.com>
2020-02-11 11:09:52 +0400

    The application configuration for working with redux.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HomePage from './home-page';
 import {BrowserRouter} from 'react-router-dom'
+import {createStore, compose, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
 
 const homePage = (
-  <BrowserRouter>
-    <HomePage />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(homePage, document.getElementById('root'));
